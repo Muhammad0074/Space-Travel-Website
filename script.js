a1e78c3fecdf3222cd54237f7760bbe85e808a88
Muhammad0074 <potamohamed0@gmail.com>
2024-08-16 15:02:21 +0300

    data storing in file done✅

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-//alert after submit
-document.getElementById('reg-form').addEventListener('submit', function(event) {
+document.getElementById('reg-form').addEventListener('submit' , function(event){
+    // form will not refresh after submittion
     event.preventDefault();
-    alert('Submitted Successfully');
-});
-
-document.getElementById('reg-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    // Collect form data
-    const formData = {
-        name: document.getElementById("name").value,
-        email: document.querySelector('input[name="email"]').value,
-        country: document.querySelector('select').value,
-        experienceLevel: document.querySelector('input[name="Experience lvl"]:checked')?.value,
-        motivation: document.getElementById('motivational-q').value,
-        consent: document.getElementById('consent').checked
-    };
-
-     // debug
-    console.log(formData);
-
+    // collect form data
+    const name = document.getElementById('name').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const country = document.querySelector('select').value;
+    const experienceLevel = document.querySelector('input[name="Experience lvl"]:checked')?.value;
+    const motivation = document.getElementById('motivational-q').value;
     
+    // create array to store data
+    const data = [
+        ["name","email","country","experience Level","motivation"],
+        [name,email,country,experienceLevel,motivation]
+    ];
+
+    // debug    
+    console.log(name, email, country, experienceLevel, motivation, consent);
+
+    // convert data into Excel sheet
+    const ws = XLSX.utils.aoa_to_sheet(data);
+
+    // create Excel file
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Registration");
+    
+    //download file 
+    XLSX.writeFile(wb, "Space_Trip_Registration.xlsx");
+    
+    //successful page after submittion
+    window.location.href = "confirm.html";
 });
